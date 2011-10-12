8c12a540c70ec8919c8c02c599a0abe7d1bca66e
paulballesty@gmail.com <paulballesty@gmail.com@78c79f6f-70aa-5706-821d-53258a698ef3>
2011-10-12 17:30:43 +0000

    Config loader y workers

--- trunk/TP-PDC/src/main/java/ar/edu/itba/it/pdc/Isecu.java
+++ trunk/TP-PDC/src/main/java/ar/edu/itba/it/pdc/Isecu.java
@@ -1,8 +1,9 @@
 package ar.edu.itba.it.pdc;
 
 import java.io.IOException;
-import java.net.InetAddress;
 
+import ar.edu.itba.it.pdc.config.ConfigLoader;
+import ar.edu.itba.it.pdc.exception.ConfigurationFileException;
 import ar.edu.itba.it.pdc.proxy.IsecuServer;
 
 public class Isecu {
@@ -10,14 +11,13 @@ public class Isecu {
 	private Isecu instance;
 	
 	public static void main(String[] args) throws IOException{
-		byte[] ipProxy = {0,0,0,0};
-		int proxyPort = 9999;
-		int configPort = 9998;
-		int portServer = 5222;
-		InetAddress interfazProxy = InetAddress.getByAddress(ipProxy);
-		InetAddress interfazServer = InetAddress.getLocalHost();
-		IsecuFactory.init(interfazServer, portServer, interfazProxy, proxyPort, configPort);
-		new IsecuServer(interfazServer, portServer, interfazProxy, proxyPort, configPort).start();
+		try {
+			ConfigLoader cl = IsecuFactory.getInstance().getConfigLoader();
+			new IsecuServer(cl.getProxyAddress(), cl.getConfigAddress(), cl.getOriginServer()).start();
+		}catch (ConfigurationFileException e) {
+			e.printStackTrace();
+			System.out.println("No se puede arrancar el proxy. Error en la configuración.");
+		}
 	}
 	
 	public Isecu getInstance(){
---
+++ trunk/TP-PDC/src/main/java/ar/edu/itba/it/pdc/exception/ConfigurationFileException.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.it.pdc.exception;
+
+@SuppressWarnings("serial")
+public class ConfigurationFileException extends Exception {
+
+	public ConfigurationFileException() {
+		super();
+	}
+
+	public ConfigurationFileException(String arg0) {
+		super(arg0);
+	}
+	
+}
