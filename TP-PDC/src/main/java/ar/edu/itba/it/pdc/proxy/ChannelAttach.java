ec8301fa8a03780485d66b8df2f9e8df86896502
paulballesty@gmail.com <paulballesty@gmail.com@78c79f6f-70aa-5706-821d-53258a698ef3>
2011-10-24 03:30:40 +0000

    Primer avance del parser (no queda funcional)

--- TP-PDC/src/main/java/ar/edu/itba/it/pdc/proxy/ChannelAttach.java
+++ TP-PDC/src/main/java/ar/edu/itba/it/pdc/proxy/ChannelAttach.java
@@ -2,25 +2,42 @@ package ar.edu.itba.it.pdc.proxy;
 
 import java.nio.ByteBuffer;
 
+import ar.edu.itba.it.pdc.proxy.parser.MessageProcessor;
+
 /**
  * Información que se attachea al socketChannel
  */
 public class ChannelAttach {
 
-	private ByteBuffer lc;
-	private ByteBuffer ls;
+	private ByteBuffer readClientBuf;
+	private ByteBuffer writeClientBuf;
+
+	private ByteBuffer serverBuf;
+	
+	private MessageProcessor processor;
 	
 	public ChannelAttach(int bufferSize) {
-		this.lc = ByteBuffer.allocate(bufferSize);
-		this.ls = ByteBuffer.allocate(bufferSize);
+		this.readClientBuf = ByteBuffer.allocate(bufferSize);
+		this.writeClientBuf = ByteBuffer.allocate(bufferSize);
+		this.serverBuf = ByteBuffer.allocate(bufferSize);
+		this.processor = new MessageProcessor();
 	}
 
-	public ByteBuffer getClientBuffer() {
-		return lc;
+	public ByteBuffer getReadClientBuffer() {
+		return readClientBuf;
+	}
+	
+	public ByteBuffer getWriteClientBuffer() {
+		return writeClientBuf;
 	}
 	
 	public ByteBuffer getServerBuffer() {
-		return ls;
+		return this.serverBuf;
 	}
 	
+	
+	public MessageProcessor getProcessor() {
+		return this.processor;
+	}
+
 }
