053d286e41da138466fcdfe414c2f54451387535
paulballesty@gmail.com <paulballesty@gmail.com@78c79f6f-70aa-5706-821d-53258a698ef3>
2011-10-25 01:32:01 +0000

    Con este llegó a conectar pero sigue tirando errores (debugging)

--- trunk/TP-PDC/src/main/java/ar/edu/itba/it/pdc/proxy/parser/XMPPMessageProcessor.java
+++ trunk/TP-PDC/src/main/java/ar/edu/itba/it/pdc/proxy/parser/XMPPMessageProcessor.java
@@ -20,46 +20,55 @@ public abstract class XMPPMessageProcessor {
 	private int lastEvent;
 	private StringBuilder buffer;
 	
+	private boolean reset = false;
+
 	protected static enum TagType {
 		MESSAGE, SUCCESS, OTHER;
 	}
-	
+
 	public XMPPMessageProcessor(ReaderFactory readerFactory) {
 		this.consumed = this.processed = this.toWrite = this.lastEvent = 0;
 		this.buffer = new StringBuilder();
 		this.readerFactory = readerFactory;
 		this.asyncReader = this.readerFactory.newAsyncReader();
 	}
-	
+
 	public void read(ByteBuffer bb, int read) {
-		bb.rewind();
-		int i;
-		for(i = 0; i < read; i++){
-			buffer.append((char) bb.get());
+		if(!this.reset) {
+			bb.rewind();
+			int i;
+			for (i = 0; i < read; i++) {
+				buffer.append((char) bb.get());
+			}
+			bb.clear();
+			process();
 		}
-		bb.clear();
-		process();
 	}
-	
+
 	public void write(ByteBuffer bb) {
-		
+
 		byte data[] = buffer.substring(0, this.toWrite).getBytes();
-		
+
 		int i = 0;
-		while(bb.position() < bb.limit() && i < this.toWrite) {
+		while (bb.position() < bb.limit() && i < this.toWrite) {
 			bb.put(data[i++]);
 		}
-		
-		this.buffer = new StringBuilder(this.buffer.substring(i, this.buffer.length()));
-		
+
+		this.buffer = new StringBuilder(this.buffer.substring(i,
+				this.buffer.length()));
+
 		System.out.println("Se escriben " + i + " para salida");
-		
+
 		this.consumed += i;
-		
+
 		this.toWrite -= i;
 		this.processed -= i;
+
+		if(this.toWrite == 0) {
+			this.resetReader();
+		}
 		
-		//Lo dejo listo para leer
+		// Lo dejo listo para leer
 		bb.flip();
 	}
 
@@ -70,33 +79,38 @@ public abstract class XMPPMessageProcessor {
 	private void process() {
 		try {
 			if (getFeeder().needMoreInput()) {
-				byte[] data = buffer.substring(processed, buffer.length()).getBytes();
+				byte[] data = buffer.substring(processed, buffer.length())
+						.getBytes();
 				getFeeder().feedInput(data, 0, data.length);
 				this.processed += data.length;
-			}else{
+			} else {
 				throw new IllegalStateException("No need input");
 			}
 
-			int event; 
+			int event;
 			while ((event = asyncReader.next()) != AsyncXMLStreamReader.EVENT_INCOMPLETE) {
-				switch(event) {
+				switch (event) {
 				case AsyncXMLStreamReader.START_DOCUMENT:
 					handleStartDocument(getCurrentLocation());
-					markLastEvent(getCurrentLocation());break;
+					markLastEvent(getCurrentLocation());
+					break;
 				case AsyncXMLStreamReader.START_ELEMENT:
 					handleStartElement(getCurrentLocation());
-					markLastEvent(getCurrentLocation());break;
+					markLastEvent(getCurrentLocation());
+					break;
 				case AsyncXMLStreamReader.END_ELEMENT:
-					handleEndElement(getCurrentLocation());break;
+					handleEndElement(getCurrentLocation());
+					break;
 				case AsyncXMLStreamReader.ATTRIBUTE:
 					handleAttribute(getCurrentLocation());
-					markLastEvent(getCurrentLocation());break;
+					markLastEvent(getCurrentLocation());
+					break;
 				default:
 					handleAnyOtherEvent(getCurrentLocation());
 				}
-				
+
 			}
-		} catch (WFCException e){
+		} catch (WFCException e) {
 			e.printStackTrace();
 		} catch (XMLStreamException e) {
 			e.printStackTrace();
@@ -107,60 +121,71 @@ public abstract class XMPPMessageProcessor {
 	private int getCurrentLocation() {
 		return asyncReader.getLocation().getCharacterOffset();
 	}
-	
+
 	public String getEventString(int vLocation) {
-		return this.buffer.substring(normalizeLocation(this.lastEvent), normalizeLocation(vLocation));
+		return this.buffer.substring(normalizeLocation(this.lastEvent),
+				normalizeLocation(vLocation));
 	}
-	
+
 	private void markToWrite(int vLocation) {
 		this.toWrite = normalizeLocation(vLocation);
 	}
-	
+
 	protected void markLastEvent(int vLocation) {
 		this.lastEvent = vLocation;
 	}
-	
+
 	protected void sendEvent(int vLocation) {
 		markLastEvent(vLocation);
 		markToWrite(vLocation);
 	}
-	
+
 	public boolean needToWrite() {
 		return this.toWrite != 0;
 	}
-	
+
 	private int normalizeLocation(int location) {
 		return location - this.consumed;
 	}
 
-	protected void resetReader() {
-		this.buffer = new StringBuilder(this.buffer.substring(normalizeLocation(getCurrentLocation()), this.buffer.length()));
-		System.out.println("Nuevo Buffer: " + this.buffer);
-		this.asyncReader = readerFactory.newAsyncReader();
+	public void markToReset() {
+		this.reset = true;
 	}
 	
+	public boolean needToReset() {
+		return this.reset;
+	}
+	
+	private void resetReader() {
+		this.buffer = new StringBuilder(this.buffer.substring(
+				normalizeLocation(getCurrentLocation()), this.buffer.length()));
+		this.toWrite = this.processed = this.consumed = 0;
+		this.asyncReader = readerFactory.newAsyncReader();
+		this.reset = false;
+	}
+
 	protected AsyncXMLStreamReader getReader() {
 		return this.asyncReader;
 	}
-	
+
 	protected TagType getTagType(String localPart) {
-		if(localPart.equalsIgnoreCase("message")) {
+		if (localPart.equalsIgnoreCase("message")) {
 			return TagType.MESSAGE;
-		}else if(localPart.equalsIgnoreCase("success")) {
+		} else if (localPart.equalsIgnoreCase("success")) {
 			return TagType.SUCCESS;
-		}else{
+		} else {
 			return TagType.OTHER;
 		}
 	}
-	
+
 	protected abstract void handleStartDocument(int vLocation);
-	
+
 	protected abstract void handleStartElement(int vLocation);
-	
+
 	protected abstract void handleAttribute(int vLocation);
-	
+
 	protected abstract void handleEndElement(int vLocation);
-	
+
 	protected abstract void handleAnyOtherEvent(int vLocation);
-	
+
 }
