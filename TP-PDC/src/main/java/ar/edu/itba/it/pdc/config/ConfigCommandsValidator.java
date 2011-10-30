b9b4ffbb2684e6fc4ebcb92b273376529ab357dc
jimenabpose <jimenabpose@78c79f6f-70aa-5706-821d-53258a698ef3>
2011-10-30 19:52:19 +0000

    Agrego el tipo "delete"

--- TP-PDC/src/main/java/ar/edu/itba/it/pdc/config/ConfigCommandsValidator.java
+++ TP-PDC/src/main/java/ar/edu/itba/it/pdc/config/ConfigCommandsValidator.java
@@ -3,8 +3,8 @@ package ar.edu.itba.it.pdc.config;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.joda.time.LocalTime;
 import org.springframework.stereotype.Component;
@@ -14,11 +14,12 @@ import ar.edu.itba.it.pdc.exception.CommandValidationException;
 @Component
 public class ConfigCommandsValidator {
 
-	public void validateBasics(HashMap<String, Object> command) {
+	public void validateBasics(Map<String, Object> command) {
 //	{"auth":["admin","admin"],"type":"assignation", "caccess":["user","qty"]}
 		List<String> validTypes = new ArrayList<String>();
 		validTypes.add("assignation");
 		validTypes.add("query");
+		validTypes.add("delete");
 		
 		boolean authIsWrong = !command.containsKey("auth") || ((List<String>)command.get("auth")).size() != 2;
 		boolean typeIsWrong = !command.containsKey("type") || !validTypes.contains(command.get("type"));
@@ -29,7 +30,7 @@ public class ConfigCommandsValidator {
 		}
 	}
 	
-	public void validateQuery(HashMap<String, Object> command) {
+	public void validateQuery(Map<String, Object> command) {
 //		{"auth":["admin","admin"],"type":"query", "parameter":"caccess"}
 		List<String> validCommands = new ArrayList<String>();
 		validCommands.add("caccess");
@@ -47,62 +48,111 @@ public class ConfigCommandsValidator {
 		}
 	}
 	
-	public void validateCaccessCommand(HashMap<String, Object> command) {
+	public void validateCaccessCommand(Map<String, Object> command) {
 //		{"auth":["admin","admin"],"type":"assignation", "caccess":["user","qty"]}
-		List<String> caccess = (List<String>)command.get("caccess");
-		Integer.valueOf(caccess.get(1));
-		if(caccess.size() != 2 || jidIsWrong(caccess.get(0))) {
-			throw new CommandValidationException();
+		if(command.get("type").equals("assignation")) {
+			List<String> caccess = (List<String>)command.get("caccess");
+			Integer.valueOf(caccess.get(1));
+			if(caccess.size() != 2 || jidIsWrong(caccess.get(0))) {
+				throw new CommandValidationException();
+			}
+		} else if(command.get("type").equals("delete")) {
+//		{"auth":["admin","admin"],"type":"delete", "caccess":"user"}
+			String caccess = (String)command.get("caccess");
+			if(jidIsWrong(caccess)) {
+				throw new CommandValidationException();
+			}
 		}
 	}
 	
-	public void validateMultiplexCommand(HashMap<String, Object> command) {
+	public void validateMultiplexCommand(Map<String, Object> command) {
+		if(command.get("type").equals("assignation")) {
 //		{"auth":["admin","admin"],"type":"assignation", "multiplex":["jid","serverto"]}
-		List<String> multiplex = (List<String>)command.get("multiplex");
-		if(multiplex.size() != 2 || jidIsWrong(multiplex.get(0))) {
-			throw new CommandValidationException();
+			List<String> multiplex = (List<String>)command.get("multiplex");
+			if(multiplex.size() != 2 || jidIsWrong(multiplex.get(0))) {
+				throw new CommandValidationException();
+			}
+		} else if(command.get("type").equals("delete")) {
+//		{"auth":["admin","admin"],"type":"assignation", "multiplex":"jid"}
+			String multiplex = (String)command.get("multiplex");
+			if(jidIsWrong(multiplex)) {
+				throw new CommandValidationException();
+			}
 		}
 	}
-
-	public void validateSilenceCommand(HashMap<String, Object> command) {
+	
+	
+	public void validateSilenceCommand(Map<String, Object> command) {
+		if(command.get("type").equals("assignation")) {
 //		{"auth":["admin","admin"],"type":"assignation", "silence":"user"}
-		if(jidIsWrong((String) command.get("silence"))) {
-			throw new CommandValidationException();
+			if(jidIsWrong((String) command.get("silence"))) {
+				throw new CommandValidationException();
+			}
+		} else if(command.get("type").equals("delete")) {
+//		{"auth":["admin","admin"],"type":"delete", "silence":"user"}
+			if(jidIsWrong((String) command.get("silence"))) {
+				throw new CommandValidationException();
+			}
 		}
 	}
-
-	public void validateFilterCommand(HashMap<String, Object> command) {
+	
+	public void validateFilterCommand(Map<String, Object> command) {
+		List<String> filter = (List<String>) command.get("filter");
+		if(command.get("type").equals("assignation")) {
 //		{"auth":["admin","admin"],"type":"assignation", "filter":["leet/hash", "user", "on/off"]}
+			if(!getValidFilters().contains(filter.get(0)) || jidIsWrong(filter.get(1)) ||
+					!getValidStatus().contains(filter.get(2))) {
+				throw new CommandValidationException();
+			}
+		} else if(command.get("type").equals("delete")) {
+//		{"auth":["admin","admin"],"type":"delete", "filter":["leet/hash", "user"]}
+			if(!getValidFilters().contains(filter.get(0)) || jidIsWrong(filter.get(1))) {
+				throw new CommandValidationException();
+			}
+		}
+	}
+	
+	private List<String> getValidFilters() {
 		List<String> validFilters = new ArrayList<String>();
 		validFilters.add("leet");
 		validFilters.add("hash");
-		
+		return validFilters;
+	}
+	
+	private List<String> getValidStatus() {
 		List<String> validStatus = new ArrayList<String>();
 		validStatus.add("on");
 		validStatus.add("off");
-		
-		List<String> filter = (List<String>) command.get("filter");
-		
-		if(!validFilters.contains(filter.get(0)) || jidIsWrong(filter.get(1)) ||
-				!validStatus.contains(filter.get(2))) {
-			throw new CommandValidationException();
-		}
+		return validStatus;
 	}
 	
-	public void validateBlacklistCommand(HashMap<String, Object> command) {
+	public void validateBlacklistCommand(Map<String, Object> command) {
 //		{"auth":["admin","admin"],"type":"assignation", "blacklist":["range","user","from","server"]}
 		List<String> blacklistCommand = (List<String>) command.get("blacklist");
 		
-		if(blacklistCommand.get(0).equals("range")) {
-			validateRangeBlacklist(blacklistCommand);
-		} else if(blacklistCommand.get(0).equals("logins")) {
-			validateLoginsBlacklist(blacklistCommand);
-		} else if(blacklistCommand.get(0).equals("ip")) {
-			validateIpBlacklist(blacklistCommand);
-		} else if(blacklistCommand.get(0).equals("net")) {
-			validateNetBlacklist(blacklistCommand);
-		} else {
-			throw new CommandValidationException();
+		if(command.get("type").equals("assignation")) {
+			if(blacklistCommand.get(0).equals("range")) {
+				validateRangeBlacklist(blacklistCommand);
+			} else if(blacklistCommand.get(0).equals("logins")) {
+				validateLoginsBlacklist(blacklistCommand);
+			} else if(blacklistCommand.get(0).equals("ip")) {
+				validateIpBlacklist(blacklistCommand);
+			} else if(blacklistCommand.get(0).equals("net")) {
+				validateNetBlacklist(blacklistCommand);
+			} else {
+				throw new CommandValidationException();
+			}
+		} else if(command.get("type").equals("delete")) {
+//			{"auth":["admin","admin"],"type":"delete", "blacklist":["range","user"]}
+			if(blacklistCommand.get(0).equals("range") || blacklistCommand.get(0).equals("logins")) {
+				validateRangeAndLoginsBlacklistDelete(blacklistCommand);
+			} else if(blacklistCommand.get(0).equals("ip")) {
+				validateIpBlacklist(blacklistCommand);
+			} else if(blacklistCommand.get(0).equals("net")) {
+				validateNetBlacklist(blacklistCommand);
+			} else {
+				throw new CommandValidationException();
+			}
 		}
 	}
 	
@@ -145,6 +195,14 @@ public class ConfigCommandsValidator {
 		}
 		Integer netmask = Integer.valueOf(command.get(2));
 		if(command.size() != 3 || (netmask < 0 || netmask > 255)) {
+			throw new CommandValidationException();
+		}
+	}
+	
+	private void validateRangeAndLoginsBlacklistDelete(List<String> command) {
+//		{"auth":["admin","admin"],"type":"delete", "blacklist":["range","user"]}
+//		{"auth":["admin","admin"],"type":"delete", "blacklist":["logins","user"]}
+		if(command.size() != 2 || jidIsWrong(command.get(1))) {
 			throw new CommandValidationException();
 		}
 	}
