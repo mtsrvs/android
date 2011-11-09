d4ccfb246bb555650cbb070c2adbd9c42bb5a9cb
paulballesty@gmail.com <paulballesty@gmail.com@78c79f6f-70aa-5706-821d-53258a698ef3>
2011-11-09 16:01:16 +0000

    Ctrl + Shift + o

--- trunk/TP-PDC/src/main/java/ar/edu/itba/it/pdc/config/ConfigHandler.java
+++ trunk/TP-PDC/src/main/java/ar/edu/itba/it/pdc/config/ConfigHandler.java
@@ -6,8 +6,6 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
-import javax.naming.ConfigurationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
